7a9c19b9011775cbd1d16cf80307475eb9f8ccc7
gabas-guedes <144071956+gabas-guedes@users.noreply.github.com>
2023-10-09 17:53:04 -0300

    Update questao 1.c

--- questao 1.c
+++ questao 1.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
 
-void main(){
+int main(){
     int ano = 0;
-    while (1){
+   
     
     scanf("%d", &ano);
 
     if ((ano >= 1896) && (ano <= 2022)) {
         if (ano % 4 == 0 && ano != 1916 && ano != 1940 && ano != 1944) {
-            printf("\nOs Jogos Olimpicos ocorreram no ano de %d", ano);
+            printf("Os Jogos Olimpicos de Verao ocorreram no ano de %d.\n", ano);
         } else if ((ano >= 1930 && ano % 2 == 0) && (ano != 1940 && ano != 1942 && ano != 1944 && ano != 1946)) {
-            printf("\nA Copa do Mundo de Futebol ocorreu no ano de %d", ano);
+            printf("A Copa do Mundo de Futebol ocorreu no ano de %d.\n", ano);
         } else {
-            printf("\nNao houve Jogos Olimpicos de Verao ou Copa do Mundo no ano de %d", ano);
+            printf("Nao houve Jogos Olimpicos de Verao ou Copa do Mundo no ano de %d.\n", ano);
         }
     } else {
-        printf("\nNao houve Jogos Olimpicos de Verao ou Copa do Mundo no ano de %d", ano);
+        printf("Nao houve Jogos Olimpicos de Verao ou Copa do Mundo no ano de %d.\n", ano);
     }
-    }
+    return 0;
 }
